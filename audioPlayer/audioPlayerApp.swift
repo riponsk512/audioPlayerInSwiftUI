d1bd838350f61163e49942e7a48b6e5c04e6610e
Ripon Seikh <34369083+riponsk512@users.noreply.github.com>
2023-08-20 22:09:02 +0530

    Initial Commit

---
+++ audioPlayer/audioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioPlayerApp.swift
+//  audioPlayer
+//
+//  Created by Ripon sk on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct audioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
